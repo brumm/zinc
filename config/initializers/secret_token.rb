734969db6ca0451a31d89dd6087c826901142366
Philipp Brumm <philipp@funkensturm.de>
2013-04-13 13:27:14 +0200

    rails new zinc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zinc::Application.config.secret_token = '24e3e5392dd9c3f5fff9b340fd019e0afca9724a746a62d15435bde5f613eb0bd5103c2aa80af168c876f576f502295f9b60a4b82c2836ff04327fdd0b158070'
