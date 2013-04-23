967df505a2cf6208d48ccd9fb45b3646a9cb1e87
Philipp Brumm <philipp@funkensturm.de>
2013-04-23 12:54:32 +0200

    set up user for roleable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,20 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
+  include Roleable::Subject
+
   attr_accessible :password_confirmation,
                   :username, :email,
                   :password
 
   validates_confirmation_of :password
-  validates_presence_of :password, on: :create
-  validates_presence_of :email
-  validates_uniqueness_of :username
-  validates_uniqueness_of :email
+  validates_presence_of     :password, on: :create
+  validates_presence_of     :email
+  validates_uniqueness_of   :username
+  validates_uniqueness_of   :email
+
+  def is? role, instance = nil
+    self.has_role? role, instance
+  end
 
 end
