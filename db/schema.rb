29fd37db6e381f8023500658194569ee3e5d5501
Philipp Brumm <philipp@funkensturm.de>
2013-04-23 12:53:30 +0200

    add roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,32 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130413145414) do
+ActiveRecord::Schema.define(:version => 20130422190937) do
+
+  create_table "applied_roles", :force => true do |t|
+    t.integer  "subject_id"
+    t.integer  "role_id"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  add_index "applied_roles", ["resource_type", "resource_id"], :name => "index_applied_roles_on_resource_type_and_resource_id"
+  add_index "applied_roles", ["subject_id"], :name => "index_applied_roles_on_subject_id"
+
+  create_table "roles", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "rooms", :force => true do |t|
+    t.string   "title"
+    t.string   "slug"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username",         :null => false
