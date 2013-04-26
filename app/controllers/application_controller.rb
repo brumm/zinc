ecacf77690539666a4497b06275884ea2928ada5
Philipp Brumm <philipp@funkensturm.de>
2013-04-26 22:12:40 +0200

    setup gon in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :setup_gon
 
   protected
 
   def not_authenticated
     redirect_to signup_path, alert: "Please login first."
+  end
+
+  def setup_gon
+    Gon.global.controller = controller_name
+    Gon.global.action     = action_name
+
+    if current_user
+      Gon.global.user     = current_user.as_json.merge({roles: current_user.roles_for_resource(nil).map(&:name)})
+    end
   end
 
 end
