ec22ed2ffb4630b52a1c2ada62aa555498096725
Philipp Brumm <philipp@funkensturm.de>
2013-05-06 19:13:02 +0200

    redirect user back to where he was after login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,13 +3,14 @@ class UserSessionsController < ApplicationController
   skip_before_filter :require_login, except: [:destroy]
 
   def new
+    session[:return_to] ||= request.referer
     redirect_back_or_to root_url if current_user
   end
 
   def create
     @user = login(params[:username], params[:password])
     if @user
-      redirect_to rooms_path, :notice => "Logged in."
+      redirect_to session[:return_to]
     else
       flash.now.alert = "Something went oops."
       render :new
